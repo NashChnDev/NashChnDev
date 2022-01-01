8714477f18d54f8cb5821886bd21207248016ba4
NASHCHNDEV <developer_chn@nashindia.com>
2022-01-01 16:40:27 +0530

    delete all files

--- Email/start.bat
+++
@@ -1,3 +0,0 @@
-cmd /c START /MIN php "C:\xampp\htdocs\Sandhar\public\Autosched.php"
-
- 
--- resources/views/layouts/footer.blade.php
+++
@@ -1,8 +0,0 @@
-  <!-- Main Footer -->
-  <footer class="main-footer" style="font-size: 0.8rem;">
-    <!-- To the right -->
-    <div class="float-right d-sm-none d-md-block">
-    </div>
-    <!-- Default to the left -->
-    <strong>Copyright &copy; 2020-2021 <a href="http://www.nashindia.com">NashInida</a>.</strong> All rights reserved.
-  </footer>
